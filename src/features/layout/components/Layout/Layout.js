04cad703b4682b226fafec35e334a54711af2763
Iván Portillo Leal <ivanportillo@me.com>
2017-05-10 17:47:04 +0200

    Change Layout to add new components and fix proptypes dependency

--- src/features/layout/components/Layout/Layout.js
+++ src/features/layout/components/Layout/Layout.js
@@ -1,10 +1,16 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import style from './Layout.css';
 
+import Header from '../Header';
+import Footer from '../Footer';
+import Content from '../Content';
 
 const Layout = ({ children }) =>
   <div className={style.mainLayout}>
-    {children}
+    <Header />
+    <Content>{children}</Content>
+    <Footer />
   </div>;
 
 Layout.propTypes = {
