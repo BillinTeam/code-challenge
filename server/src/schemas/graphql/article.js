0bd434a41bfad498a862026038a2d58f6a60aece
Sergio <sergiazow@gmail.com>
2018-07-09 16:23:38 +0200

    Added article mutation + Improved article fetching
    There was only a field in the schema "articles" which always returned an array, so fetching a single article meant to transform the request response from an array to a single element.
    
    Also a mutation has been added for the articles.

--- server/src/schemas/graphql/article.js
+++ server/src/schemas/graphql/article.js
@@ -13,7 +13,7 @@ const articleFields = {
   author: { type: GraphQLString },
   content: { type: GraphQLString },
   excerpt: { type: GraphQLString },
-  
+
   published: { type: GraphQLBoolean },
   tags: { type: new GraphQLList(GraphQLString) },
   title: { type: GraphQLString },
@@ -22,57 +22,58 @@ const articleFields = {
 const articleType = new GraphQLObjectType({
   name: 'Article',
   description: 'This represents a Article',
-  fields: () => ({
-    ...articleFields,
-    id: { type: GraphQLString },
-  }),
+  fields: () => ({ ...articleFields, id: { type: GraphQLString }, }),
 });
+
+
 const articleInputType = new GraphQLInputObjectType({
   name: 'ArticleInputType',
   description: 'User payload definition',
   fields: () => (articleFields),
 });
+
+
 const Query = new GraphQLObjectType({
   name: 'Query',
   description: 'This is a root query',
   fields: () => ({
     articles: {
       type: new GraphQLList(articleType),
+      resolve() { return Article.find(); },
+    },
+    article: {
+      type: articleType,
       args: {
-        id: {
-          type: GraphQLString
-        }
+        id: { type: GraphQLString }
       },
-      resolve(_, { id }) {
-        if (typeof id != "undefined") {
-          return Article.find({ _id: id });
-        }
-        return Article.find();
-      },
+      resolve(_, { id }) { return Article.findOne({ _id: id }) },
     },
   }),
 });
 
-const ArticleSchema = new GraphQLSchema({
-  query: Query,
-  mutation: new GraphQLObjectType({
-    name: 'Mutation',
-    fields: () => ({
-      createArticle: {
-        type: articleType,
-        args: {
-          input: {
-            type: new GraphQLNonNull(articleInputType),
-          },
-        },
-        resolve: async (rootValue, { input }) => {
-          console.log(input);
-          let newArticle = new Article(input);
-          return newArticle.save();
+
+
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: () => ({
+    createArticle: {
+      type: articleType,
+      args: {
+        input: {
+          type: new GraphQLNonNull(articleInputType),
         },
       },
-    })
+      resolve: async (rootValue, { input }) => {
+        console.log(input);
+        let newArticle = new Article(input);
+        return newArticle.save();
+      },
+    },
   })
+})
+const ArticleSchema = new GraphQLSchema({
+  query: Query,
+  mutation: Mutation
 });
 
 export default ArticleSchema;
--- server/src/server.js
+++ server/src/server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', true);
   if (req.method === 'OPTIONS') {
     res.end();
   }
