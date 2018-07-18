6f2c4e421901a1146d9dc84e202bd2d0b93e62ff
Angel <as.gonzalezp@gmail.com>
2018-07-18 21:33:02 +0200

    Add header inline style

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+const style = {
+  textAlign: 'center',
+  color: 'red',
+  marginTop: 20,
+  marginBottom: 20,
+  fontSize: 30,
+};
+
 export const Header = () => (
-  <div>I am a Header</div>
+  <div style={style}>BILLIN CODE CHALLENGE</div>
 );
