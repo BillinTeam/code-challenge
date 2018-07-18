63b997358350737cc8fcc6939a64dc8b1d793d3b
Angel <as.gonzalezp@gmail.com>
2018-07-18 21:12:16 +0200

    Move api request from app to cards-container

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,35 +1,14 @@
-import React, { Component } from 'react';
-import request from '../api/request';
-import { ARTICLES_QUERY } from '../api/queries';
+import React from 'react';
 import { Header } from './header';
 import { CardsContainer } from './cards-container';
 import { Footer } from './footer';
 
-export default class App extends Component {
-  // definition
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-    };
-  }
+const App = () => (
+  <div className="App">
+    <Header />
+    <CardsContainer />
+    <Footer />
+  </div>
+);
 
-  // lifecycle
-  componentWillMount() {
-    request(ARTICLES_QUERY).then(response => {
-      this.setState({ articles: response.data.articles });
-    });
-  }
-
-  // Renders
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <CardsContainer />
-        <Footer />
-        <pre>{JSON.stringify(this.state.articles, null, 2)}</pre>
-      </div>
-    );
-  }
-}
+export default App;
--- src/containers/cards-container.js
+++ src/containers/cards-container.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import request from '../api/request';
+import { ARTICLES_QUERY } from '../api/queries';
+import Card from '../components/card';
 
-export const CardsContainer = () => (
-  <div>I am a CardsContainer</div>
-);
+export class CardsContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+    };
+  }
+
+  componentWillMount() {
+    request(ARTICLES_QUERY)
+      .then(response => {
+        this.setState({ articles: response.data.articles });
+      });
+  }
+
+  render() {
+    const cards = this.state.articles.map(art => (<Card {...art} />));
+    return (<Fragment>
+      { cards }
+    </Fragment>);
+  }
+}
